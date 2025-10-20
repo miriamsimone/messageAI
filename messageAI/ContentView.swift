81fe7f6234e6b0f37f8202a9b908b2ca226c4ca0
Miriam Simone <max.flander@gmail.com>
2025-10-20 11:11:49 -0700

    Initial Commit

---
+++ messageAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  messageAI
+//
+//  Created by Miriam Flander on 10/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ messageAI/messageAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  messageAIApp.swift
+//  messageAI
+//
+//  Created by Miriam Flander on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct messageAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
